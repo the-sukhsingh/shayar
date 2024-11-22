742ebff745887acb5432292d853d5821c7f97da0
Sukhjit <sukhaji65@gmail.com>
2024-11-22 18:39:02 +0530

    Update Footer component styles for responsiveness; remove global font size

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <footer className="rounded-none border-none outline-none">
-    <div className="flex justify-center items-center h-16 rounded-none text-2xl">
+    <div className="flex justify-center items-center h-16 rounded-none  md:text-2xl">
       <p>Made By Sukhjit Singh with 💖</p>
     </div>
    </footer>
